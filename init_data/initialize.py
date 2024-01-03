a3d1fe3eaa6cc3c45568f0dd2e44cd9f363827bc
zhilin he <zhilinhe@zhilins-MBP.lan>
2024-01-02 21:47:11 -0800

    import journal

--- init_data/initialize.py
+++ init_data/initialize.py
@@ -160,5 +160,5 @@ class build_graph:
 if __name__ == "__main__":
     # Init = init_graph(hugegraphClient.HugeGraphClient)
     # Init.run()
-    imp = build_graph(entity=['author', 'paper'], graph_connector=hugegraphClient.HugeGraphClient)
+    imp = build_graph(entity=['journal'], graph_connector=hugegraphClient.HugeGraphClient)
     imp.run()
